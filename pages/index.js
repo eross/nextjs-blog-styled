892697a4eedcd85b05f2285e3ec46bde44455da9
Eric Ross <epr@rossmbox.com>
2025-10-04 21:00:08 -0700

    change intro again

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Newby to vercel]</p>
+        <p>[Newby to vercel, really.]</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
